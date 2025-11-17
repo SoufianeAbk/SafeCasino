b894247486ce1c6a416b038074127bb560e8c132
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-11-17 17:58:41 +0100

    Deleted Duplicate File.

--- ViewModels/Errorviewmodel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SafeCasino.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
