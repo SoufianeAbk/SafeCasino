f4376f95b5da7523a733f33032f0147e2b11be6d
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-11-17 17:30:13 +0100

    Quick Fix.

--- Program.cs
+++ Program.cs
@@ -2,17 +2,11 @@ using SafeCasino.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Register custom services
 builder.Services.AddScoped<IGameApiService, GameApiService>();
 builder.Services.AddSingleton<ILocalizationService, LocalizationService>();
-
-// Add memory cache for better performance
 builder.Services.AddMemoryCache();
 
-// Add session support if needed
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -20,7 +14,6 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-// Configure cookie policy
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
     options.CheckConsentNeeded = context => false;
@@ -29,11 +22,9 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
