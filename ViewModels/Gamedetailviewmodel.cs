ba74749feddd07a3e0ce30ce076da64ec7d600a4
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-11-17 02:24:18 +0100

    Qucik Models Update.

--- ViewModels/Gamedetailviewmodel.cs
+++ ViewModels/Gamedetailviewmodel.cs
@@ -1,6 +1,12 @@
-﻿namespace SafeCasino.ViewModels
+﻿using SafeCasino.Models;
+
+namespace SafeCasino.ViewModels
 {
-    public class Gamedetailviewmodel
+    public class GameDetailViewModel
     {
+        public Game Game { get; set; } = new Game();
+        public List<Game> RelatedGames { get; set; } = new List<Game>();
+        public string CurrentLanguage { get; set; } = "nl";
+        public bool IsDemo { get; set; } = true;
     }
 }
--- ViewModels/Gamelistviewmodel.cs
+++ ViewModels/Gamelistviewmodel.cs
@@ -1,6 +1,19 @@
-﻿namespace SafeCasino.ViewModels
+﻿using SafeCasino.Models;
+
+namespace SafeCasino.ViewModels
 {
-    public class Gamelistviewmodel
+    public class GameListViewModel
     {
+        public List<Game> Games { get; set; } = new List<Game>();
+        public FilterOptions Filters { get; set; } = new FilterOptions();
+        public List<string> AvailableProviders { get; set; } = new List<string>();
+        public List<GameCategory> AvailableCategories { get; set; } = new List<GameCategory>();
+        public int TotalGames { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string CurrentLanguage { get; set; } = "nl";
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
     }
 }
