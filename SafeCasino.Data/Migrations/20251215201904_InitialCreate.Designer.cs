599ea75ec352f3e625f13a30504de0b542d9af8b
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-12-15 21:27:43 +0100

    Nieuwe Migration + Database Aangemaakt.

--- SafeCasino.Data/Migrations/20251215201904_InitialCreate.Designer.cs
+++ SafeCasino.Data/Migrations/20251215201904_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SafeCasino.Data.Data;
 
@@ -11,9 +12,11 @@ using SafeCasino.Data.Data;
 namespace SafeCasino.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215201904_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
